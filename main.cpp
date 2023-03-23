6b2e75cac462ef75deea3eea773085b3e763518f
Jakub Nowak <jakub.nowak@zst.kolbuszowa.pl>
2023-03-23 11:32:52 +0100

    break

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // napisz program w c++ wykorzystujący funkcje break jesli i=0, i < 10, i++ pętla ma się zatrzymać na liczbie 4
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        if (i == 4) {
+            break;
+        }
+        cout << i << "\n";
+    }
+    return 0;
+}
